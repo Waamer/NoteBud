e70d85c07972a16ee8d1093a1e3b40856eddd90f
waamer <108493771+Waamer@users.noreply.github.com>
2024-07-13 19:13:07 -0400

    Added Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
@@ -39,6 +40,7 @@ export default function RootLayout({
           </Providers>
         </ThemeProvider>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
