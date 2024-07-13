47bd75a5edd703caed9945f7e992d069f7e0d914
waamer <108493771+Waamer@users.noreply.github.com>
2024-07-13 16:51:43 -0400

    Trying to fix gradient on mobile

--- components/bot.tsx
+++ components/bot.tsx
@@ -3,13 +3,13 @@ import Spline from '@splinetool/react-spline/next';
 export default function Bot() {
   return (
   <>
-    <div className='hidden sm:block'>
-      <Spline
+    <div className='hidden sm:block select-none'>
+      <Spline className='select-none'
         scene="https://prod.spline.design/9eIyi3iaEX6qYFG9/scene.splinecode" 
       />
     </div>
-    <div className='sm:hidden'>
-      <Spline
+    <div className='sm:hidden select-none'>
+      <Spline className='select-none'
         scene="https://prod.spline.design/llZLmjtoFEby-mTp/scene.splinecode" 
       />
     </div>
