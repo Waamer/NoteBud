d02738bc324baecf25a012c9eb1e628073581e40
waamer <108493771+Waamer@users.noreply.github.com>
2024-07-13 01:36:15 -0400

    Mobile performance testing

--- components/bot.tsx
+++ components/bot.tsx
@@ -2,10 +2,17 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function Bot() {
   return (
-    <div>
+  <>
+    <div className='hidden sm:block'>
       <Spline
         scene="https://prod.spline.design/9eIyi3iaEX6qYFG9/scene.splinecode" 
       />
     </div>
+    <div className='sm:hidden'>
+      <Spline
+        scene="https://prod.spline.design/llZLmjtoFEby-mTp/scene.splinecode" 
+      />
+    </div>
+  </>
   );
 }
