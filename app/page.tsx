82d378532e8ec3d20c337e9b4ee6598454d12a66
waamer <108493771+Waamer@users.noreply.github.com>
2024-07-13 18:27:46 -0400

    fixed homepage lagging/gradeints not working

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,11 @@ export default function LandingPage() {
 
   return (<>
     <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-4 pb-4 pt-20 sm:pt-28">
-      <div className="flex flex-col lg:flex-row-reverse border-2 rounded-lg">
-          <div className="-z-40 absolute inset-x-0 m-auto mt-24 lg:mt-10 h-80 max-w-5xl bg-gradient-to-r from-rose-400 to-[#b061ff] blur-[118px]"></div>
+      <div className="flex flex-col lg:flex-row-reverse border-2 rounded-lg relative overflow-visible sm:overflow-visible">
+        <div className="absolute inset-0 -z-10 sm:hidden">
+          <div className="h-[94%] mt-6 max-w-5xl bg-gradient-to-tr from-rose-400/70 via-rose-600/70 dark:via-rose-900/70 to-[#b061ff]/70 dark:to-[#C084FC]/70 blur-[40px]"></div>
+        </div>
+          <div className="hidden sm:block -z-40 absolute inset-x-0 m-auto mt-10 h-full mx-8 sm:mx-auto sm:h-80 max-w-5xl bg-gradient-to-tr from-rose-400 via-rose-600 dark:via-rose-900 to-[#b061ff] dark:to-[#C084FC] blur-[118px]"></div>
           <div className="flex-1 rounded-none rounded-t-lg lg:rounded-none lg:rounded-e-lg flex flex-col justify-center lg:justify-end items-center h-64 lg:h-auto lg:w-fit bg-rose-200/50 dark:bg-rose-950/40">
             <Bot />
             <div className="flex gap-1 p-1.5 font-medium mr-auto bg-rose-200/40 dark:bg-rose-950/30 rounded-tr-lg">
